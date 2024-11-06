6e5e4e93f3e1b7172ea95d93f899fcd37c263477
Koushik27042001 <koushikchakrabortykgp@gmail.com>
2024-11-06 19:19:29 +0530

    userModule

--- src/app/core/components/user/user-enrty/user-enrty.component.ts
+++ src/app/core/components/user/user-enrty/user-enrty.component.ts
@@ -1,12 +1,55 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-user-enrty',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './user-enrty.component.html',
+//   styleUrl: './user-enrty.component.scss'
+// })
+// export class UserEnrtyComponent {
+
+// }
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MaterialModule } from '../../../models/material.model';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-user-enrty',
+  selector: 'app-user-entry',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule],
   templateUrl: './user-enrty.component.html',
-  styleUrl: './user-enrty.component.scss'
+  styleUrl: './user-enrty.component.scss'// corrected to styleUrls
 })
-export class UserEnrtyComponent {
+export class UserEntryComponent {
+  userForm: FormGroup;
 
+  constructor(
+    private dialogRef: MatDialogRef<UserEntryComponent>,
+    private fb: FormBuilder
+  ) {
+    this.userForm = this.fb.group({
+      role: ['', Validators.required],
+      description: ['', Validators.required],
+    });
+  }
+
+  onSave(): void {
+    if (this.userForm.valid) {
+      this.dialogRef.close(this.userForm.value);
+    }
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
 }
+
