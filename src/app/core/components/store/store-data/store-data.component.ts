38d6b3593d2d8cc6caa70b2a9bfe894db3532178
Sunil <sunil.cm@dhineu.com>
2024-11-07 15:49:35 +0530

    merged

---
+++ src/app/core/components/store/store-data/store-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-store-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './store-data.component.html',
+  styleUrl: './store-data.component.scss'
+})
+export class StoreDataComponent {
+
+}
--- src/app/pages/store/store.component.ts
+++ src/app/pages/store/store.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { StoreDataComponent } from "../../core/components/store/store-data/store-data.component";
 
 @Component({
   selector: 'app-store',
   standalone: true,
-  imports: [],
+  imports: [StoreDataComponent],
   templateUrl: './store.component.html',
   styleUrl: './store.component.scss'
 })
